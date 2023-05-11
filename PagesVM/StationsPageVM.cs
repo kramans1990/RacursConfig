6accc0eee93579ea9bf18692bb7058b47ed9f2f5
Roman <kramans1990@outlook.com>
2023-05-12 00:29:28 +0300

    fix : Правки страницы станций

--- PagesVM/StationsPageVM.cs
+++ PagesVM/StationsPageVM.cs
@@ -9,6 +9,9 @@ using Newtonsoft.Json;
 using System.Windows;
 using RacursLib;
 using System.Windows.Controls;
+using System.Collections.ObjectModel;
+using RacursConfig.Controls;
+using System.Linq;
 
 namespace Client.PagesVM
 {   
@@ -110,19 +113,7 @@ namespace Client.PagesVM
         }
 
 
-        private List<string> _WarningMessages;
-        public List<string> WarningMessages
-        {
-            get
-            {
-                return _WarningMessages;
-            }
-            set
-            {
-                _WarningMessages = value;
-                OnPropertyChanged(nameof(WarningMessages));
-            }
-        }
+     
 
         //public RelayCommand AddCommand
         //{
@@ -182,7 +173,7 @@ namespace Client.PagesVM
             EditorVisibility = Visibility.Hidden;
             httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri(App.baseUrl);
-            StationEditor = new Station { Name = "Новая Станция", Description = "Описание", Angle = 20};
+            StationEditor = new Station { Name = "Новая Станция", Description = "Описание станции", Angle = 1};
             //StationEditor.XGsk = gsk.X;
             //StationEditor.YGsk = gsk.Y;
             //StationEditor.ZGsk = gsk.Z;
@@ -192,25 +183,37 @@ namespace Client.PagesVM
             //AddStationToDataBase(StationEditor);
             getStations();
             SelectedStation = new Station();
-            WarningMessages = new List<string>(); 
+            Messages = new ObservableCollection<string>();
             AddCommand = new RelayCommand(x => Add());
             CancelCommand = new RelayCommand(x => Cancel());
             DeleteCommand = new RelayCommand(x => DeleteStation(x));
-            SaveCommand = new RelayCommand(x => Save());
+            SaveCommand = new RelayCommand(x => Save(), p => canSave());
             EditCommand = new RelayCommand(x => Edit());
-            Satellite satellite = new Satellite();
-            M = new RacursLib.LibMath.Matrix3(1, 1, 1, 1, 1, 1, 1, 1, 1);
-            M.N22 = 500;
-            //TestText = "123";
-            //List<Itnp> Itnsp = new List<Itnp>();
-            //Itnp itnp1 = new Itnp {Date = DateTime.Now, SatelliteId=1,X=10,Y=20,Z=20,RadialSpeed=30,Range=1000 };
-            //Itnp itnp2 = new Itnp { Date = DateTime.Now.AddSeconds(1), SatelliteId = 1, X = 20, Y = 20, Z = 220, RadialSpeed = 34, Range = 5 };
-            //Itnsp.Add(itnp1);
-            //Itnsp.Add(itnp2);
-            // HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "/api/itnps");
-            // getItnp();
+        
+          
+        
 
 
+        }
+
+        private bool canSave()
+        {
+            DependencyObject do_ = (Application.Current.MainWindow);
+            var frame = FindVisualChildren<Frame>((do_));
+            List<NumberField> fieldsNum = FindVisualChildren<NumberField>(frame.First()).ToList();
+            var find_FalseNum = fieldsNum.Where(p => p.IsValid == false);
+            bool result = find_FalseNum.Count() == 0 ? true : false;
+
+            if (!result)
+            {
+                return result;
+            }
+
+            List<TextField> fieldsText = FindVisualChildren<TextField>(frame.First()).ToList();
+            var find_FalseText = fieldsText.Where(p => p.IsValid == false);
+            bool resultText = find_FalseText.Count() == 0 ? true : false;
+
+            return resultText;
         }
 
         private void Edit()
@@ -238,9 +241,7 @@ namespace Client.PagesVM
 
         private void Add()
         {
-            //TestText = "qwe";
-            var t = M;
-            t.N22 = 500;
+           
             EditorVisibility = Visibility.Visible;
             mode = "Add";
             StationEditor = new Station { Name = "Новая Станция", Description = "Описание" };
@@ -258,14 +259,14 @@ namespace Client.PagesVM
                 var response = await httpClient.SendAsync(request);
                 if (response.IsSuccessStatusCode)
                 {
-                    WarningMessages.Add(GetTimeLabel() + "Станция Удалена из БД");
+                    Messages.Add(GetTimeLabel() + "Станция Удалена из БД");
                     getStations();
                     
                 }
-                else { WarningMessages.Add(response.ReasonPhrase); }
+                else { Messages.Add(response.ReasonPhrase); }
             }
             catch (Exception exception){
-                WarningMessages.Add( GetTimeLabel() +  exception.Message);
+                Messages.Add( GetTimeLabel() +  exception.Message);
             }
         }
         private async void EditStation(Station station)
@@ -283,39 +284,18 @@ namespace Client.PagesVM
                 if (response.IsSuccessStatusCode)
                 {
                     EditorVisibility = Visibility.Hidden;
-                    WarningMessages.Add(GetTimeLabel() + "Изменения успешно сохранены в БД");
+                    Messages.Add(GetTimeLabel() + "Изменения успешно сохранены в БД");
                     getStations();
                     
                 }
-                else { WarningMessages.Add(GetTimeLabel() + response.ReasonPhrase); }
+                else { Messages.Add(GetTimeLabel() + response.ReasonPhrase); }
             }
             catch (Exception exception)
             {
-                WarningMessages.Add(GetTimeLabel() + exception.Message);
+                Messages.Add(GetTimeLabel() + exception.Message);
             }
         }
 
-        //private  async void getItnp() {
-        //    try
-        //    {   
-        //        string paramss = "?dateTimeFrom=" + DateTime.Now.AddDays(-10).ToString("yyyy-MM-ddTHH:mm:ss") + "&" +
-        //                          "dateTimeTo=" + DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss") + "&satelliteId=1";
-
-
-
-        //        var response = await httpClient.GetAsync("/api/Itnps" + paramss);
-        //        if (response.IsSuccessStatusCode)
-        //        {
-        //            var result = response.Content.ReadAsStringAsync();
-        //            var t  = JsonConvert.DeserializeObject<List<Itnp>>(result.Result);
-        //        }
-        //        else { WarningMessages.Add(response.ReasonPhrase); }
-        //    }
-        //    catch (Exception exception)
-        //    {
-        //        WarningMessages.Add(GetTimeLabel() + exception.Message);
-        //    }
-        //}
         private async void getStations()
         {
             try
@@ -324,17 +304,25 @@ namespace Client.PagesVM
                 if (response.IsSuccessStatusCode)
                 {
                     var result = response.Content.ReadAsStringAsync();
-                    WarningMessages.Add(GetTimeLabel() + "Запрос списка станций");
+                    Messages.Add(GetTimeLabel() + "Запрос списка станций");
                     Stations = JsonConvert.DeserializeObject<List<Station>>(result.Result);                   
                 }
-                else { WarningMessages.Add(response.ReasonPhrase); }
+                else { Messages.Add(response.ReasonPhrase); }
             }
             catch (Exception exception) {
-                WarningMessages.Add(GetTimeLabel() +  exception.Message);
+                Messages.Add(GetTimeLabel() +  exception.Message);
             }
         }
         private async void AddStationToDataBase(Station station)
         {
+            double x = station.XGsk;
+            double y = station.YGsk;
+            double z = station.ZGsk;
+            double r = Math.Sqrt(x * x + y * y + z * z);
+            if (r < 6371) {
+                Messages.Add(GetTimeLabel() + " Ошибка : станция с такими координатами будет находится ниже поверхности Земли");
+                return;
+            }
             try
             {
                 RacursLib.LibMath.Vector geo = CoordTransform.Greenvich_To_Geo(new RacursLib.LibMath.Vector(station.XGsk, station.YGsk, station.ZGsk));
@@ -342,22 +330,22 @@ namespace Client.PagesVM
                 station.Longitude = Math.Round(geo.Y * 180 / Math.PI, 4);
                 station.Altitude = geo.Z * 1e-3;
                 string content = JsonConvert.SerializeObject(station);
-                //httpClient.DefaultRequestHeaders = Hea
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "/api/stations");
+               
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, route);
                 request.Content = new StringContent(content, Encoding.UTF8, "application/json");
                 var response = await httpClient.SendAsync(request);
                 if (response.IsSuccessStatusCode)
                 {
                     EditorVisibility = Visibility.Hidden;
-                    WarningMessages.Add(GetTimeLabel() + "Станция сохранена в БД");
+                    Messages.Add(GetTimeLabel() + "Станция сохранена в БД");
                     getStations();
                    
                 }
-                else { WarningMessages.Add(GetTimeLabel() + response.ReasonPhrase); }
+                else { Messages.Add(GetTimeLabel() + response.ReasonPhrase); }
             }
             catch (Exception exception)
             {
-                WarningMessages.Add(GetTimeLabel() + exception.Message);
+                Messages.Add(GetTimeLabel() + exception.Message);
             }
         }
         private string GetTimeLabel() {
